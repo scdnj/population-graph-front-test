bbb89d6cc6a844e6132c6ea95531177f2bb78f4f
sasakura1994 <kanazawa01211123@gmail.com>
2023-11-02 00:08:38 +0900

    test(jest-config): :hammer: `@storybook/testing-vue3`のドキュメントの通りにjest.setup.jsを設定
    https://github.com/storybookjs/testing-vue3

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,11 @@ module.exports = {
   roots: ['<rootDir>'],
   moduleNameMapper: {},
   moduleFileExtensions: ['ts', 'js', 'json', 'vue'],
-  transform: { '^.+\\.(t|j)sx?$': ['@swc/jest'] },
+  transform: {
+    '^.+\\.(t|j)sx?$': ['@swc/jest'],
+    '^.+\\.vue$': '@vue/vue3-jest', // Update to match your installed version
+  },
   testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'],
   collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}'],
+  setupFiles: ['<rootDir>/tests/jest.setup.js'],
 };
---
+++ tests/jest.setup.js
@@ -0,0 +1,4 @@
+import { setProjectAnnotations } from '@storybook/testing-vue3';
+import * as globalStorybookConfig from '../.storybook/preview'; // path of your preview.js file
+
+setProjectAnnotations(globalStorybookConfig);
