e327d88c334937191ac1320decd6fa16fa673ef2
sasakura1994 <kanazawa01211123@gmail.com>
2023-11-04 19:55:52 +0900

    test(temp): :white_check_mark: リファクタリングのためにテストを書いた
    リファクタリング用の脆いテストなのでリファクタリング出来たら消す。

--- src/App.stories.ts
+++ src/App.stories.ts
@@ -1,5 +1,7 @@
+import { within } from '@storybook/testing-library';
 import App from './App.vue';
 import type { Meta, StoryObj } from '@storybook/vue3';
+import { wait } from './scripts/util/wait';
 
 type Story = StoryObj<typeof App>;
 type M = Meta<typeof App>;
@@ -12,3 +14,13 @@ const meta: M = {
 export default meta;
 
 export const Default: Story = {};
+
+export const 選択済み: Story = {
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    (await canvas.findByLabelText('北海道')).click();
+    (await canvas.findByLabelText('東京都')).click();
+    // 画面が更新されるまで待つ
+    await wait(100);
+  },
+};
