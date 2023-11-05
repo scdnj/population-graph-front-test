2a60ac8e48e4a21a13a0c2a21e0721b1494aa38d
sasakura1994 <kanazawa01211123@gmail.com>
2023-11-05 10:42:09 +0900

    test: :white_check_mark: 人口取得のメソッドのキャッシュが効いていることを確認

--- src/scripts/composables/useComposition/useComposition.test.ts
+++ src/scripts/composables/useComposition/useComposition.test.ts
@@ -8,6 +8,10 @@ jest.spyOn(Api.prototype, 'getPrefectures').mockResolvedValue([
   { prefCode: 151, prefName: 'aaaaa' },
 ]);
 
+beforeEach(() => {
+  jest.clearAllMocks();
+});
+
 it('fetchPrefecturesするとprefecturesに値が入る', async () => {
   const hook = useComposition();
   expect(hook.prefectures.value.length).toEqual(0);
@@ -35,6 +39,54 @@ it('prefecturesをcheckするとcompositionに値が入る', async () => {
   // 1ms待たないとcompositionに値が入らない
   await wait(1);
 
+  expect(hook.compositions.value).toEqual([
+    {
+      composition: [
+        { data: [{ value: 1, year: 1960 }], label: '総人口' },
+        { data: [{ value: 1, year: 1960 }], label: 'あいうえお人口' },
+      ],
+      prefName: '都道府県',
+    },
+  ]);
+});
+
+it('同じprefCodeを何度もチェックしても値を取得していたら再取得しない', async () => {
+  const spy = jest.spyOn(Api.prototype, 'getComposition').mockImplementation(async () => {
+    return [
+      { label: '総人口', data: [{ value: 1, year: 1960 }] },
+      { label: 'あいうえお人口', data: [{ value: 1, year: 1960 }] },
+    ];
+  });
+  const hook = useComposition();
+  await hook.fetchPrefectures();
+  const prefCode = 15;
+  expect(spy).not.toHaveBeenCalled();
+  hook.checkedPrefectures.value[prefCode] = true;
+  await wait(1);
+  expect(spy).toHaveBeenCalledTimes(1);
+  hook.checkedPrefectures.value[prefCode] = false;
+  await wait(1);
+  hook.checkedPrefectures.value[prefCode] = true;
+  await wait(1);
+  expect(spy).toHaveBeenCalledTimes(1);
+});
+
+it('二回目のチェックでも人口に値が入っている', async () => {
+  jest.spyOn(Api.prototype, 'getComposition').mockImplementation(async () => {
+    return [
+      { label: '総人口', data: [{ value: 1, year: 1960 }] },
+      { label: 'あいうえお人口', data: [{ value: 1, year: 1960 }] },
+    ];
+  });
+  const hook = useComposition();
+  await hook.fetchPrefectures();
+  const prefCode = 15;
+  hook.checkedPrefectures.value[prefCode] = true;
+  await wait(1);
+  hook.checkedPrefectures.value[prefCode] = false;
+  await wait(1);
+  hook.checkedPrefectures.value[prefCode] = true;
+  await wait(1);
   expect(hook.compositions.value).toEqual([
     {
       composition: [
