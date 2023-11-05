d7fde4aa4cd676f1a98b6bc5500a9c7f2fcd8f00
sasakura1994 <kanazawa01211123@gmail.com>
2023-11-05 21:42:30 +0900

    feat: :children_crossing: ボタンを押しやすく、タップ領域を見やすく

--- src/components/domain/composition/PrefecturesCompositionViewer/PrefecturesCompositionViewer.vue
+++ src/components/domain/composition/PrefecturesCompositionViewer/PrefecturesCompositionViewer.vue
@@ -14,11 +14,11 @@ onMounted(fetchPrefectures);
   <div>
     <Section title="都道府県">
       <div class="flex flex-wrap text-lg font-bold">
-        <div v-for="prefecture in prefectures" :key="prefecture.prefCode" class="w-24">
+        <div v-for="prefecture in prefectures" :key="prefecture.prefCode" class="w-1/3 sm:w-24">
           <Checkbox
             v-model="checkedPrefectures[prefecture.prefCode]"
             :label="prefecture.prefName"
-            class="p-1"
+            class="sm:p-1 py-2 pl-1 m-1 sm:m-0 bg-blue-500/20"
           />
         </div>
       </div>
