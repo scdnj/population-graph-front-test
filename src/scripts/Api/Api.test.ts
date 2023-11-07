f0b117020480b0e9e67ed44ea7ae9cbb52da8d8e
sasakura1994 <kanazawa01211123@gmail.com>
2023-11-08 01:58:44 +0900

    feat: :goal_net: APIサービスのエラーハンドリング実装

--- src/scripts/Api/Api.test.ts
+++ src/scripts/Api/Api.test.ts
@@ -6,6 +6,44 @@ jest.useFakeTimers({ advanceTimers: true });
 const axios = _axios as jest.MockedObject<typeof _axios>;
 axios.create.mockImplementation(() => axios);
 
+// https://opendata.resas-portal.go.jp/docs/api/v1/detail/index.htmlより
+const mockApiError = (status: 400 | 403 | 404 | 429 | 500 | 404_2) => {
+  const mock = (data: unknown) => {
+    axios.get.mockResolvedValueOnce({ data, status: 200 });
+  };
+  switch (status) {
+    case 400:
+      mock('400');
+      break;
+    case 403:
+      mock({
+        description: '',
+        message: 'Forbidden.',
+        statusCode: '403',
+      });
+      break;
+    case 404:
+      mock({
+        description: 'The requested URL /404 was not found on this server.',
+        message: "404. That's an error.",
+        statusCode: '404',
+      });
+      break;
+
+    case 404_2:
+      mock('404');
+      break;
+    case 429:
+      mock({
+        message: null,
+      });
+      break;
+    case 500:
+      mock('500');
+      break;
+  }
+};
+
 beforeAll(() => {
   axios.get.mockImplementation(async (path: string) => {
     const wrap = <T>(data: T) => ({ data: { message: null, result: data } });
@@ -50,6 +88,26 @@ describe('getPrefectures', () => {
         prefName: '北海道',
       },
     ]);
+  });
+  describe('エラー', () => {
+    it.each(([400, 403, 404, 429, 500, 404] as const).map((status) => [status]))(
+      '%d',
+      async (status) => {
+        const api = new Api();
+        mockApiError(status);
+        await expect(api.getPrefectures()).rejects.toThrow(status.toString());
+      },
+    );
+    it('404がstringでエラーが返ってくるパターンもテスト書いておく', async () => {
+      const api = new Api();
+      mockApiError(404_2);
+      await expect(api.getPrefectures()).rejects.toThrow('404');
+    });
+    it('なんだかわからないエラーもcatchする', async () => {
+      const api = new Api();
+      axios.get.mockRejectedValueOnce(new Error('unknown error'));
+      await expect(api.getPrefectures()).rejects.toThrow('unknown error');
+    });
   });
 });
 
--- src/scripts/Api/index.ts
+++ src/scripts/Api/index.ts
@@ -10,6 +10,28 @@ type ApiReturn<T> = {
 
 const key = getApiKey();
 
+type Error403 = {
+  description: string;
+  message: string;
+  statusCode: '403';
+};
+
+type Error404 = {
+  description: string;
+  message: string;
+  statusCode: '404';
+};
+// | '404';
+// 本当はこのパターンもあるけどstringで返ってくるものは他でcatchしてるので一旦省略
+
+type Error429 = { message: null };
+
+type Error400 = '400';
+
+type Error500 = '500';
+
+type ApiErrorResponses = Error403 | Error404 | Error429 | Error400 | Error500;
+
 export type Prefecture = {
   prefCode: number;
   prefName: string;
@@ -32,6 +54,47 @@ export interface ApiInterface {
   getComposition: (prefCode: number) => Promise<Composition>;
 }
 
+const deepEqual = (a: unknown, b: unknown): boolean => {
+  return JSON.stringify(a) === JSON.stringify(b);
+};
+
+type ErrorStatus = 400 | 403 | 404 | 429 | 500;
+class ApiErrorImpl extends Error {
+  statusCode: ErrorStatus;
+  description: string;
+  constructor(status: ErrorStatus) {
+    super(status.toString());
+    this.statusCode = status;
+    if (status === 400) {
+      this.description = 'パラメータの設定が間違っています。開発者にお問い合わせください';
+    } else if (status === 403) {
+      this.description = 'APIキーが間違っています。開発者にお問い合わせください';
+    } else if (status === 404) {
+      this.description = 'データが見つかりませんでした。開発者にお問い合わせください';
+    } else if (status === 429) {
+      this.description = 'リクエストが多すぎます。しばらくしてから再度お試しください';
+    } else if (status === 500) {
+      this.description = 'サーバーエラーが発生しています。しばらくしてから再度お試しください';
+    } else {
+      this.description = '想定していないエラーが発生しました。開発者にお問い合わせください';
+    }
+  }
+}
+
+export type ApiError = ApiErrorImpl;
+
+function catchError<T>(e: T | ApiErrorResponses): asserts e is T {
+  if (typeof e === 'string') {
+    throw new ApiErrorImpl(Number(e) as 400 | 500 | 404);
+  } else if (deepEqual(e, { message: null })) {
+    throw new ApiErrorImpl(429);
+  } else if ((e as unknown as Error403).statusCode === '403') {
+    throw new ApiErrorImpl(403);
+  } else if ((e as unknown as Error404).statusCode === '404') {
+    throw new ApiErrorImpl(404);
+  }
+}
+
 class ApiImpl implements ApiInterface {
   private readonly axios = axios.create({
     headers: { 'X-API-KEY': key },
@@ -42,7 +105,11 @@ class ApiImpl implements ApiInterface {
    * 都道府県一覧を返す
    */
   async getPrefectures(): Promise<Prefecture[]> {
-    const { data } = await this.axios.get<ApiReturn<Prefecture[]>>(`/prefectures`);
+    const { data } = await this.axios.get<ApiReturn<Prefecture[]> | ApiErrorResponses>(
+      `/prefectures`,
+    );
+    catchError(data);
+
     return data.result;
   }
 
@@ -51,9 +118,10 @@ class ApiImpl implements ApiInterface {
    * @param prefCode 都道府県コード
    */
   async getComposition(prefCode: number): Promise<Composition> {
-    const { data } = await this.axios.get<ApiComposition>(
+    const { data } = await this.axios.get<ApiComposition | ApiErrorResponses>(
       `/population/composition/perYear?cityCode=-&prefCode=${prefCode}`,
     );
+    catchError(data);
     return {
       総人口: getCompositionData(data, '総人口'),
       年少人口: getCompositionData(data, '年少人口'),
